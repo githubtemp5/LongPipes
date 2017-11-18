8d6a612e1bd62b2fb394c7b5731284c37de3f044
githubtemp5 <githubtemp@gmail.com>
2017-11-18 17:18:47 +0000

    Changed if statements for Pipe1, Pipe2, and Pipe3

--- LongPipes/src/longpipes/Main.java
+++ LongPipes/src/longpipes/Main.java
@@ -14,12 +14,13 @@ import java.util.Scanner;
  * @author up826133
  */
 public class Main {
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
-      
+
         print("Enter the length of the pipes in metres: ");
         double length = myScanner.nextDouble();
         print("Enter the diameter in inches: ");
@@ -36,42 +37,46 @@ public class Main {
         String chemRes = myScanner.nextLine();
         print("Quanity of Pipe: ");
         int quantity = myScanner.nextInt();
-        
-        ArrayList<Pipe> pipes= new ArrayList<Pipe>();
-        
+
+        ArrayList<Pipe> pipes = new ArrayList<Pipe>();
+
         //Deciding pipe type
-        
-        if(colour==0&& grade>=1 && grade <=3&& insul.equals("N")&& outerInforce.equals("N")){
-        //Pipe1
-        
-        pipes.add(new Pipe1(length, diameter, grade, chemRes, quantity));
+        if (insul.equals("N") && outerInforce.equals("N")) {
+            //Pipe1
+            if (colour == 0) {
+                if (grade >= 1 && grade <= 3) {
+                    pipes.add(new Pipe1(length, diameter, grade, chemRes, quantity));
+                }
+            }
+            //Pipe2
+            if (colour == 1) {
+                if (grade >= 2 && grade <= 4) {
+                    pipes.add(new Pipe2(length, diameter, grade, chemRes, quantity));
+                }
+            }
+            //Pipe3
+            if (colour == 2) {
+                if (grade >= 2 && grade <= 5) {
+                    pipes.add(new Pipe3(length, diameter, grade, chemRes, quantity));
+                }
+            }
         }
-        if(colour ==1 && grade >=2 && grade <=4 && insul.equals("N")&& outerInforce.equals("N")){
-        //Pipe2
-        pipes.add(new Pipe2(length, diameter, grade, chemRes, quantity));
+
+        if (colour == 2 && grade >= 2 && grade <= 5 && insul.equals("Y") && outerInforce.equals("N")) {
+            //Pipe4
+            pipes.add(new Pipe4(length, diameter, grade, chemRes, quantity));
         }
-        if(colour ==2 && grade >=2 && grade <=5 && insul.equals("N")&& outerInforce.equals("N")){
-        //Pipe3
-        pipes.add(new Pipe3(length, diameter, grade, chemRes, quantity));
-        }
-        if(colour ==2 && grade >=2 && grade <=5 && insul.equals("Y")&& outerInforce.equals("N")){
-        //Pipe4
-        pipes.add(new Pipe4(length, diameter, grade, chemRes, quantity));
-        }
-        if(colour ==2 && grade >=3 && grade <=5 && insul.equals("Y")&& outerInforce.equals("Y")){
-        //Pipe5
-        pipes.add(new Pipe5(length, diameter, grade, chemRes, quantity));
-        }
-        else{
+        if (colour == 2 && grade >= 3 && grade <= 5 && insul.equals("Y") && outerInforce.equals("Y")) {
+            //Pipe5
+            pipes.add(new Pipe5(length, diameter, grade, chemRes, quantity));
+        } else {
             System.out.println("Invalid Pipe Type");
         }
-        
+
     }
 
-    
-    public static void print(String text){
+    public static void print(String text) {
         System.out.println(text);
     }
 
-    
 }
