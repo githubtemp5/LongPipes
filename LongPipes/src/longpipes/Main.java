09c2a08eba68ea1f0274b277e6a94817ca79ef48
githubtemp5 <githubtemp@gmail.com>
2017-11-19 15:37:34 +0000

    Added comments and deleted test sout statements

--- LongPipes/src/longpipes/Main.java
+++ LongPipes/src/longpipes/Main.java
@@ -32,30 +32,22 @@ public class Main {
         do {
             print("Enter the length of the pipes in metres: ");
             length = myScanner.nextDouble();
-            System.out.println(length+"");
             print("Enter the diameter in inches: ");
             diameter = myScanner.nextDouble();
-            System.out.println(diameter+"");
             print("Enter the grade: ");
             grade = myScanner.nextInt();
-            System.out.println(grade+"");
             print("Colour [0], [1], [2]");
             colour = myScanner.nextInt();
-            System.out.println(colour+"");
             print("Insulation: Y/N ");
-            do {
+            do {                                    //solve issue where readline reads an empty character when nextInt or nextDouble is execute beforehand
                 insul = myScanner.nextLine();
             } while (insul.equals(""));
-            System.out.println(insul);
             print("Outer Reinforcement: Y/N");
             outerInforce = myScanner.nextLine();
-            System.out.println(outerInforce);
             print("Chemical Resistance: Y/N");
             chemRes = myScanner.nextLine();
-            System.out.println(chemRes);
             print("Quanity of Pipe: ");
             quantity = myScanner.nextInt();
-            System.out.println(quantity);
         } while (error);
         ArrayList<Pipe> pipes = new ArrayList<Pipe>();
 
@@ -90,6 +82,7 @@ public class Main {
         if (colour == 2 && grade >= 3 && grade <= 5 && insul.equalsIgnoreCase("Y") && outerInforce.equalsIgnoreCase("Y")) {
             pipes.add(new Pipe5(length, diameter, grade, chemRes, quantity));
         } 
+        
         else {
             System.out.println("Invalid Pipe Type");
         }
