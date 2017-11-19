5f6053fd3923c7ebd4ef9187b90e41697c91e4eb
githubtemp5 <githubtemp@gmail.com>
2017-11-19 14:43:23 +0000

    do while loop for input error checking

--- LongPipes/src/longpipes/Main.java
+++ LongPipes/src/longpipes/Main.java
@@ -20,24 +20,33 @@ public class Main {
      */
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
-
-        print("Enter the length of the pipes in metres: ");
-        double length = myScanner.nextDouble();
-        print("Enter the diameter in inches: ");
-        double diameter = myScanner.nextDouble();
-        print("Enter the grade: ");
-        int grade = myScanner.nextInt();
-        print("Colour [0], [1], [2]");
-        int colour = myScanner.nextInt();
-        print("Insulation: Y/N ");
-        String insul = myScanner.nextLine();
-        print("Outer Reinforcement: Y/N");
-        String outerInforce = myScanner.nextLine();
-        print("Chemical Resistance: Y/N");
-        String chemRes = myScanner.nextLine();
-        print("Quanity of Pipe: ");
-        int quantity = myScanner.nextInt();
-
+        boolean error = false;
+        double length;
+        double diameter;
+        int grade;
+        int colour;
+        String insul;
+        String outerInforce;
+        String chemRes;
+        int quantity;
+        do {
+            print("Enter the length of the pipes in metres: ");
+            length = myScanner.nextDouble();
+            print("Enter the diameter in inches: ");
+            diameter = myScanner.nextDouble();
+            print("Enter the grade: ");
+            grade= myScanner.nextInt();
+            print("Colour [0], [1], [2]");
+            colour= myScanner.nextInt();
+            print("Insulation: Y/N ");
+            insul = myScanner.nextLine();
+            print("Outer Reinforcement: Y/N");
+            outerInforce = myScanner.nextLine();
+            print("Chemical Resistance: Y/N");
+            chemRes = myScanner.nextLine();
+            print("Quanity of Pipe: ");
+            quantity = myScanner.nextInt();
+        } while (error);
         ArrayList<Pipe> pipes = new ArrayList<Pipe>();
 
         //Deciding pipe type
@@ -65,7 +74,7 @@ public class Main {
         if (colour == 2 && grade >= 2 && grade <= 5 && insul.equals("Y") && outerInforce.equals("N")) {
             pipes.add(new Pipe4(length, diameter, grade, chemRes, quantity));
         }
-        
+
         //Pipe5
         if (colour == 2 && grade >= 3 && grade <= 5 && insul.equals("Y") && outerInforce.equals("Y")) {
             pipes.add(new Pipe5(length, diameter, grade, chemRes, quantity));
