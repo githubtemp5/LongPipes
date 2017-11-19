179b9c8d65125e2d35a241b6930d3abce43e5d26
githubtemp5 <githubtemp@gmail.com>
2017-11-19 15:42:48 +0000

    Added comments and deleted test sout statements

--- LongPipes/src/pipes/Pipe1.java
+++ LongPipes/src/pipes/Pipe1.java
@@ -8,7 +8,5 @@ public class Pipe1 extends Pipe {
 
     public Pipe1(double _length, double _diameter, int _grade, String _chemicalRes, int _quantity) {
         super(_length,_diameter, _grade,0,"N","N", _chemicalRes, _quantity);
-        
-
     }
 }
