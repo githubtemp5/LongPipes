ebf60ee2583f6906eea8aeb8bc80c35364da0b83
githubtemp5 <30862041+githubtemp5@users.noreply.github.com>
2017-12-04 19:38:12 +0000

    Add files via upload

--- LongPipes/src/pipes/Pipe5.java
+++ LongPipes/src/pipes/Pipe5.java
@@ -6,14 +6,6 @@ package pipes;
  */
 public class Pipe5 extends Pipe {
 
-    /**
-     *
-     * @param _length
-     * @param _diameter
-     * @param _grade
-     * @param _chemicalRes
-     * @param _quantity
-     */
     public Pipe5(double _length, double _diameter, int _grade, boolean _chemicalRes, int _quantity) {
         super(_length, _diameter, _grade, 2, true, true, _chemicalRes, _quantity);
 
