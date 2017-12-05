bbd4594b7f7df4b2428e19cb322cefcd43cc5d06
up826133 <up826133@myport.ac.uk>
2017-12-05 18:26:06 +0000

    Added comments for all

--- LongPipes/src/pipes/Pipe4.java
+++ LongPipes/src/pipes/Pipe4.java
@@ -6,6 +6,15 @@ package pipes;
  */
 public class Pipe4 extends Pipe {
 
+    /**
+     *
+     * @param _length input of pipe length
+     * @param _diameter input of pipe diameter
+     * @param _grade inputted pipe grade
+     * @param _chemicalRes inputted chemical resistance (true or false) (present
+     * or not)
+     * @param _quantity inputted quantity of the pipe
+     */
     public Pipe4(double _length, double _diameter, int _grade, boolean _chemicalRes, int _quantity) {
         super(_length, _diameter, _grade, 2, true, false, _chemicalRes, _quantity);
 
